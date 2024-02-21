3faa2d1cddcf81350b11992d22e8614fd2c74f04
ugoovitus <favourvitus9@gmail.com>
2024-02-21 14:05:56 +0100

    still updating auth from supabase

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -29,9 +29,9 @@
 // }
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 import { createServerClient } from '@supabase/ssr'
-import type { Handle } from '@sveltejs/kit'
+// import type { Handle } from '@sveltejs/kit'
 
-export const handle: Handle = async ({ event, resolve }) => {
+export const handle = async ({ event, resolve }) => {
     event.locals.supabase = createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
         cookies: {
             get: (key) => event.cookies.get(key),
