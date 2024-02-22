3549128211e8630900024e55484fcdd62a65780a
ugoovitus <favourvitus9@gmail.com>
2024-02-22 07:51:56 +0100

    still updating auth from supbase1\a

--- src/lib/BlogContainer.svelte
+++ src/lib/BlogContainer.svelte
@@ -3,3 +3,16 @@
 <!-- This will be the main blog container wrapper -->
 
 <main />
+<!-- // // See https://kit.svelte.dev/docs/types#app
+// // for information about these interfaces
+// declare global {
+//     namespace App {
+//         interface Error { }
+//         interface Locals { any }
+//         interface PageData { }
+//         interface PageState { }
+//         interface Platform { }
+//     }
+// }
+
+// export { }; -->
