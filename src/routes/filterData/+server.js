5ab5b06ec87eb3340763d3b0c8c8d197f15e80ee
ugoovitus <favourvitus9@gmail.com>
2024-02-23 14:50:54 +0100

    stilll on it5

--- src/routes/filterData/+server.js
+++ src/routes/filterData/+server.js
@@ -2,8 +2,12 @@
 
 // import { supabase, getServiceSupabase } from "$lib/supabaseClient";
 // @ts-ignore
+// import supabase from '@supabase/ssr';
+// import getServiceSupabase from '@supabase/ssr';
+// const { supabase } = supabase;
+// const { getServiceSupabase } = gtt;
 
-import { supabase } from "@supabase/ssr";
+// import { supabase } from "@supabase/ssr";
 // import { supabase, getServiceSupabase } from "@supabase/ssr";
 // import { json } from '@sveltejs/kit';
 // @ts-ignore
@@ -23,7 +27,9 @@ let transformedArray = []
 
 let emptySalesArray = false
 
-console.log('supabase is:', supabase)
+// console.log('supabase is:', supabase)
+let supabase = createClient('https://rncxlbomipatsyzygigd.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuY3hsYm9taXBhdHN5enlnaWdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE0NTEzMzIsImV4cCI6MTk5NzAyNzMzMn0.9MOwHgtuMuP7G6D6of9b16CC3V6-kzYjYn15AL3Yv1Q')
 
 let gtt = createClient('https://rncxlbomipatsyzygigd.supabase.co',
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuY3hsYm9taXBhdHN5enlnaWdkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MTQ1MTMzMiwiZXhwIjoxOTk3MDI3MzMyfQ.N6IquPFx-Gp8Qh8k5uEUcXaPf49SeefNDcEPr3j4Nuc')
