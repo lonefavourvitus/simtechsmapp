7ac3c22825f8d55d6a42b2267e2adada86f6d5bb
ugoovitus <favourvitus9@gmail.com>
2024-02-23 15:12:35 +0100

    are we there

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -31,7 +31,7 @@
 //         },
 //     })
 // }
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
+// import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 import { createServerClient } from '@supabase/ssr'
 // import type { Handle } from '@sveltejs/kit'
 
@@ -39,7 +39,7 @@ import { createServerClient } from '@supabase/ssr'
 export const handle = async ({ event, resolve }) => {
     // @ts-ignore
 
-    event.locals.supabase = createBrowserClient("https://rncxlbomipatsyzygigd.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuY3hsYm9taXBhdHN5enlnaWdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE0NTEzMzIsImV4cCI6MTk5NzAyNzMzMn0.9MOwHgtuMuP7G6D6of9b16CC3V6-kzYjYn15AL3Yv1Q", {
+    event.locals.supabase = createServerClient("https://rncxlbomipatsyzygigd.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuY3hsYm9taXBhdHN5enlnaWdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE0NTEzMzIsImV4cCI6MTk5NzAyNzMzMn0.9MOwHgtuMuP7G6D6of9b16CC3V6-kzYjYn15AL3Yv1Q", {
         cookies: {
             get: (key) => event.cookies.get(key),
             /**
